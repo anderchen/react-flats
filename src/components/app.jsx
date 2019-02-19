f653bf6948992c647d24a947529fbf090c0f3ca5
Anderson Chen <ander.chen00@gmail.com>
2019-02-19 08:07:36 -0300

    React google maps

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+import Map from './map';
+
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div>
         <div className="flat-list"></div>
-        <div className="map-container"></div>
+        <div className="map-container">
+          <Map />
+        </div>
       </div>
     );
   }
